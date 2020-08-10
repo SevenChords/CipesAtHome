73fc0813b1bf936200f5101af14788254b9e3677
Malleo <seanowens@comcast.net>
2020-08-10 19:10:14 -0400

    Item data structs and enums, starting inventory handling

--- inventory.c
+++ inventory.c
@@ -6,226 +6,137 @@
 #define INVENTORY_MAX_SIZE 21
 #define NUM_ITEMS 107
 
-static const char *ITEM_STRING[] = {
-	"POW_Block",
-	"Icicle_Pop",
-	"Fright_Mask",
-	"Spicy_Soup",
-	"Ink_Pasta",
-	"Couples_Cake",
-	"Point_Swap",
-	"Space_Food",
-	"Ultra_Shroom",
-	"Golden_Leaf",
-	"Cake_Mix",
-	"Courage_Shell",
-	"Courage_Meal",
-	"Thunder_Bolt",
-	"Thunder_Rage",
-	"Koopa_Tea",
-	"Turtley_Leaf",
-	"Koopasta",
-	"Koopa_Bun",
-	"Spicy_Pasta",
-	"Omelette_Meal",
-	"Mushroom",
-	"Shroom_Fry",
-	"Shroom_Crepe",
-	"Shroom_Cake",
-	"Shroom_Steak",
-	"Shroom_Roast",
-	"Shooting_Star",
-	"Gold_Bar",
-	"Gold_Bar_x_3",
-	"Life_Shroom",
-	"Dizzy_Dial",
-	"Shroom_Broth",
-	"Ice_Storm",
-	"Coconut_Bomb",
-	"Coco_Candy",
-	"Spite_Pouch",
-	"Mistake",
-	"Dried_Shroom",
-	"Inn_Coupon",
-	"Choco_Cake",
-	"Trial_Stew",
-	"Slow_Shroom",
-	"Gradual_Syrup",
-	"Super_Shroom",
-	"HP_Drain",
-	"Tasty_Tonic",
-	"Stopwatch",
-	"Spaghetti",
-	"Inky_Sauce",
-	"Whacka_Bump",
-	"Horsetail",
-	"Repel_Cape",
-	"Boos_Sheet",
-	"Power_Punch",
-	"Keel_Mango",
-	"Poison_Shroom",
-	"Dried_Bouquet",
-	"Mystery",
-	"Zess_Cookie",
-	"Zess_Special",
-	"Zess_Dynamite",
-	"Zess_Tea",
-	"Zess_Dinner",
-	"Zess_Deluxe",
-	"Zess_Frappe",
-	"Sleepy_Sheep",
-	"Love_Pudding",
-	"Honey_Candy",
-	"Honey_Shroom",
-	"Honey_Super",
-	"Honey_Ultra",
-	"Honey_Syrup",
-	"Egg_Bomb",
-	"Volt_Shroom",
-	"Electro_Pop",
-	"Peach_Tart",
-	"Peachy_Peach",
-	"Fire_Pop",
-	"Fire_Flower",
-	"Mystic_Egg",
-	"Mr_Softener",
-	"Fruit_Parfait",
-	"Fresh_Juice",
-	"Healthy_Salad",
-	"Meteor_Meal",
-	"Hot_Dog",
-	"Ruin_Powder",
-	"Mango_Delight",
-	"Mini_Mr_Mini",
-	"Mousse_Cake",
-	"Maple_Shroom",
-	"Maple_Super",
-	"Maple_Ultra",
-	"Maple_Syrup",
-	"Fried_Egg",
-	"Heartful_Cake",
-	"Coconut",
-	"Snow_Bunny",
-	"Earth_Quake",
-	"Hot_Sauce",
-	"Jelly_Shroom",
-	"Jelly_Super",
-	"Jelly_Ultra",
-	"Jelly_Candy",
-	"Jammin_Jelly",
-	"Fresh_Pasta"
+typedef enum Alpha_Sort Alpha_Sort;
+typedef enum Type_Sort Type_Sort;
+typedef struct Item Item;
+
+/*====================NOTES====================
+- Items are interpreted based on the Alpha_Sort enumerator value. A function can later be written to conver this to a string.
+  - This negates the need to allocate memory to handle strings, which will reduce issues in the future.
+  - Using the alpha key as the main identifier also negates the need to have a function to obtain the alpha key along with the type key.
+- Each item in the inventory is represented with a struct which contains both its alphabetical and type sort keys, again using
+  the alphabetical key as the main item identifier.
+====================     ====================*/
+
+Item items[] = {
+	{POW_Block, POW_Block_t},
+	{Icicle_Pop, Icicle_Pop_t},
+	{Fright_Mask, Fright_Mask_t},
+	{Spicy_Soup, Spicy_Soup_t},
+	{Ink_Pasta, Ink_Pasta_t},
+	{Couples_Cake, Couples_Cake_t},
+	{Point_Swap, Point_Swap_t},
+	{Space_Food, Space_Food_t},
+	{Ultra_Shroom, Ultra_Shroom_t},
+	{Golden_Leaf, Golden_Leaf_t},
+	{Cake_Mix, Cake_Mix_t},
+	{Courage_Shell, Courage_Shell_t},
+	{Courage_Meal, Courage_Meal_t},
+	{Thunder_Bolt, Thunder_Bolt_t},
+	{Thunder_Rage, Thunder_Rage_t},
+	{Koopa_Tea, Koopa_Tea_t},
+	{Turtley_Leaf, Turtley_Leaf_t},
+	{Koopasta, Koopasta_t},
+	{Koopa_Bun, Koopa_Bun_t},
+	{Spicy_Pasta, Spicy_Pasta_t},
+	{Omelette_Meal, Omelette_Meal_t},
+	{Mushroom, Mushroom_t},
+	{Shroom_Fry, Shroom_Fry_t},
+	{Shroom_Crepe, Shroom_Crepe_t},
+	{Shroom_Cake, Shroom_Cake_t},
+	{Shroom_Steak, Shroom_Steak_t},
+	{Shroom_Roast, Shroom_Roast_t},
+	{Shooting_Star, Shooting_Star_t},
+	{Gold_Bar, Gold_Bar_t},
+	{Gold_Bar_x_3, Gold_Bar_x_3_t},
+	{Life_Shroom, Life_Shroom_t},
+	{Dizzy_Dial, Dizzy_Dial_t},
+	{Shroom_Broth, Shroom_Broth_t},
+	{Ice_Storm, Ice_Storm_t},
+	{Coconut_Bomb, Coconut_Bomb_t},
+	{Coco_Candy, Coco_Candy_t},
+	{Spite_Pouch, Spite_Pouch_t},
+	{Mistake, Mistake_t},
+	{Dried_Shroom, Dried_Shroom_t},
+	{Inn_Coupon, Inn_Coupon_t},
+	{Choco_Cake, Choco_Cake_t},
+	{Trial_Stew, Trial_Stew_t},
+	{Slow_Shroom, Slow_Shroom_t},
+	{Gradual_Syrup, Gradual_Syrup_t},
+	{Super_Shroom, Super_Shroom_t},
+	{HP_Drain, HP_Drain_t},
+	{Tasty_Tonic, Tasty_Tonic_t},
+	{Stopwatch, Stopwatch_t},
+	{Spaghetti, Spaghetti_t},
+	{Inky_Sauce, Inky_Sauce_t},
+	{Whacka_Bump, Whacka_Bump_t},
+	{Horsetail, Horsetail_t},
+	{Repel_Cape, Repel_Cape_t},
+	{Boos_Sheet, Boos_Sheet_t},
+	{Power_Punch, Power_Punch_t},
+	{Keel_Mango, Keel_Mango_t},
+	{Poison_Shroom, Poison_Shroom_t},
+	{Dried_Bouquet, Dried_Bouquet_t},
+	{Mystery, Mystery_t},
+	{Zess_Cookie, Zess_Cookie_t},
+	{Zess_Special, Zess_Special_t},
+	{Zess_Dynamite, Zess_Dynamite_t},
+	{Zess_Tea, Zess_Tea_t},
+	{Zess_Dinner, Zess_Dinner_t},
+	{Zess_Deluxe, Zess_Deluxe_t},
+	{Zess_Frappe, Zess_Frappe_t},
+	{Sleepy_Sheep, Sleepy_Sheep_t},
+	{Love_Pudding, Love_Pudding_t},
+	{Honey_Candy, Honey_Candy_t},
+	{Honey_Shroom, Honey_Shroom_t},
+	{Honey_Super, Honey_Super_t},
+	{Honey_Ultra, Honey_Ultra_t},
+	{Honey_Syrup, Honey_Syrup_t},
+	{Egg_Bomb, Egg_Bomb_t},
+	{Volt_Shroom, Volt_Shroom_t},
+	{Electro_Pop, Electro_Pop_t},
+	{Peach_Tart, Peach_Tart_t},
+	{Peachy_Peach, Peachy_Peach_t},
+	{Fire_Pop, Fire_Pop_t},
+	{Fire_Flower, Fire_Flower_t},
+	{Mystic_Egg, Mystic_Egg_t},
+	{Mr_Softener, Mr_Softener_t},
+	{Fruit_Parfait, Fruit_Parfait_t},
+	{Fresh_Juice, Fresh_Juice_t},
+	{Healthy_Salad, Healthy_Salad_t},
+	{Meteor_Meal, Meteor_Meal_t},
+	{Hot_Dog, Hot_Dog_t},
+	{Ruin_Powder, Ruin_Powder_t},
+	{Mango_Delight, Mango_Delight_t},
+	{Mini_Mr_Mini, Mini_Mr_Mini_t},
+	{Mousse_Cake, Mousse_Cake_t},
+	{Maple_Shroom, Maple_Shroom_t},
+	{Maple_Super, Maple_Super_t},
+	{Maple_Ultra, Maple_Ultra_t},
+	{Maple_Syrup, Maple_Syrup_t},
+	{Fried_Egg, Fried_Egg_t},
+	{Heartful_Cake, Heartful_Cake_t},
+	{Coconut, Coconut_t},
+	{Snow_Bunny, Snow_Bunny_t},
+	{Earth_Quake, Earth_Quake_t},
+	{Hot_Sauce, Hot_Sauce_t},
+	{Jelly_Shroom, Jelly_Shroom_t},
+	{Jelly_Super, Jelly_Super_t},
+	{Jelly_Ultra, Jelly_Ultra_t},
+	{Jelly_Candy, Jelly_Candy_t},
+	{Jammin_Jelly, Jammin_Jelly_t},
+	{Fresh_Pasta, Fresh_Pasta_t}
 };
 
-struct Items items [] = {
-	{POW_Block_a, POW_Block_t, "POW_Block"},
-	{Icicle_Pop_a, Icicle_Pop_t, "Icicle_Pop"},
-	{Fright_Mask_a, Fright_Mask_t, "Fright_Mask"},
-	{Spicy_Soup_a, Spicy_Soup_t, "Spicy_Soup"},
-	{Ink_Pasta_a, Ink_Pasta_t, "Ink_Pasta"},
-	{Couples_Cake_a, Couples_Cake_t, "Couples_Cake"},
-	{Point_Swap_a, Point_Swap_t, "Point_Swap"},
-	{Space_Food_a, Space_Food_t, "Space_Food"},
-	{Ultra_Shroom_a, Ultra_Shroom_t, "Ultra_Shroom"},
-	{Golden_Leaf_a, Golden_Leaf_t, "Golden_Leaf"},
-	{Cake_Mix_a, Cake_Mix_t, "Cake_Mix"},
-	{Courage_Shell_a, Courage_Shell_t, "Courage_Shell"},
-	{Courage_Meal_a, Courage_Meal_t, "Courage_Meal"},
-	{Thunder_Bolt_a, Thunder_Bolt_t, "Thunder_Bolt"},
-	{Thunder_Rage_a, Thunder_Rage_t, "Thunder_Rage"},
-	{Koopa_Tea_a, Koopa_Tea_t, "Koopa_Tea"},
-	{Turtley_Leaf_a, Turtley_Leaf_t, "Turtley_Leaf"},
-	{Koopasta_a, Koopasta_t, "Koopasta"},
-	{Koopa_Bun_a, Koopa_Bun_t, "Koopa_Bun"},
-	{Spicy_Pasta_a, Spicy_Pasta_t, "Spicy_Pasta"},
-	{Omelette_Meal_a, Omelette_Meal_t, "Omelette_Meal"},
-	{Mushroom_a, Mushroom_t, "Mushroom"},
-	{Shroom_Fry_a, Shroom_Fry_t, "Shroom_Fry"},
-	{Shroom_Crepe_a, Shroom_Crepe_t, "Shroom_Crepe"},
-	{Shroom_Cake_a, Shroom_Cake_t, "Shroom_Cake"},
-	{Shroom_Steak_a, Shroom_Steak_t, "Shroom_Steak"},
-	{Shroom_Roast_a, Shroom_Roast_t, "Shroom_Roast"},
-	{Shooting_Star_a, Shooting_Star_t, "Shooting_Star"},
-	{Gold_Bar_a, Gold_Bar_t, "Gold_Bar"},
-	{Gold_Bar_x_3_a, Gold_Bar_x_3_t, "Gold_Bar_x_3"},
-	{Life_Shroom_a, Life_Shroom_t, "Life_Shroom"},
-	{Dizzy_Dial_a, Dizzy_Dial_t, "Dizzy_Dial"},
-	{Shroom_Broth_a, Shroom_Broth_t, "Shroom_Broth"},
-	{Ice_Storm_a, Ice_Storm_t, "Ice_Storm"},
-	{Coconut_Bomb_a, Coconut_Bomb_t, "Coconut_Bomb"},
-	{Coco_Candy_a, Coco_Candy_t, "Coco_Candy"},
-	{Spite_Pouch_a, Spite_Pouch_t, "Spite_Pouch"},
-	{Mistake_a, Mistake_t, "Mistake"},
-	{Dried_Shroom_a, Dried_Shroom_t, "Dried_Shroom"},
-	{Inn_Coupon_a, Inn_Coupon_t, "Inn_Coupon"},
-	{Choco_Cake_a, Choco_Cake_t, "Choco_Cake"},
-	{Trial_Stew_a, Trial_Stew_t, "Trial_Stew"},
-	{Slow_Shroom_a, Slow_Shroom_t, "Slow_Shroom"},
-	{Gradual_Syrup_a, Gradual_Syrup_t, "Gradual_Syrup"},
-	{Super_Shroom_a, Super_Shroom_t, "Super_Shroom"},
-	{HP_Drain_a, HP_Drain_t, "HP_Drain"},
-	{Tasty_Tonic_a, Tasty_Tonic_t, "Tasty_Tonic"},
-	{Stopwatch_a, Stopwatch_t, "Stopwatch"},
-	{Spaghetti_a, Spaghetti_t, "Spaghetti"},
-	{Inky_Sauce_a, Inky_Sauce_t, "Inky_Sauce"},
-	{Whacka_Bump_a, Whacka_Bump_t, "Whacka_Bump"},
-	{Horsetail_a, Horsetail_t, "Horsetail"},
-	{Repel_Cape_a, Repel_Cape_t, "Repel_Cape"},
-	{Boos_Sheet_a, Boos_Sheet_t, "Boos_Sheet"},
-	{Power_Punch_a, Power_Punch_t, "Power_Punch"},
-	{Keel_Mango_a, Keel_Mango_t, "Keel_Mango"},
-	{Poison_Shroom_a, Poison_Shroom_t, "Poison_Shroom"},
-	{Dried_Bouquet_a, Dried_Bouquet_t, "Dried_Bouquet"},
-	{Mystery_a, Mystery_t, "Mystery"},
-	{Zess_Cookie_a, Zess_Cookie_t, "Zess_Cookie"},
-	{Zess_Special_a, Zess_Special_t, "Zess_Special"},
-	{Zess_Dynamite_a, Zess_Dynamite_t, "Zess_Dynamite"},
-	{Zess_Tea_a, Zess_Tea_t, "Zess_Tea"},
-	{Zess_Dinner_a, Zess_Dinner_t, "Zess_Dinner"},
-	{Zess_Deluxe_a, Zess_Deluxe_t, "Zess_Deluxe"},
-	{Zess_Frappe_a, Zess_Frappe_t, "Zess_Frappe"},
-	{Sleepy_Sheep_a, Sleepy_Sheep_t, "Sleepy_Sheep"},
-	{Love_Pudding_a, Love_Pudding_t, "Love_Pudding"},
-	{Honey_Candy_a, Honey_Candy_t, "Honey_Candy"},
-	{Honey_Shroom_a, Honey_Shroom_t, "Honey_Shroom"},
-	{Honey_Super_a, Honey_Super_t, "Honey_Super"},
-	{Honey_Ultra_a, Honey_Ultra_t, "Honey_Ultra"},
-	{Honey_Syrup_a, Honey_Syrup_t, "Honey_Syrup"},
-	{Egg_Bomb_a, Egg_Bomb_t, "Egg_Bomb"},
-	{Volt_Shroom_a, Volt_Shroom_t, "Volt_Shroom"},
-	{Electro_Pop_a, Electro_Pop_t, "Electro_Pop"},
-	{Peach_Tart_a, Peach_Tart_t, "Peach_Tart"},
-	{Peachy_Peach_a, Peachy_Peach_t, "Peachy_Peach"},
-	{Fire_Pop_a, Fire_Pop_t, "Fire_Pop"},
-	{Fire_Flower_a, Fire_Flower_t, "Fire_Flower"},
-	{Mystic_Egg_a, Mystic_Egg_t, "Mystic_Egg"},
-	{Mr_Softener_a, Mr_Softener_t, "Mr_Softener"},
-	{Fruit_Parfait_a, Fruit_Parfait_t, "Fruit_Parfait"},
-	{Fresh_Juice_a, Fresh_Juice_t, "Fresh_Juice"},
-	{Healthy_Salad_a, Healthy_Salad_t, "Healthy_Salad"},
-	{Meteor_Meal_a, Meteor_Meal_t, "Meteor_Meal"},
-	{Hot_Dog_a, Hot_Dog_t, "Hot_Dog"},
-	{Ruin_Powder_a, Ruin_Powder_t, "Ruin_Powder"},
-	{Mango_Delight_a, Mango_Delight_t, "Mango_Delight"},
-	{Mini_Mr_Mini_a, Mini_Mr_Mini_t, "Mini_Mr_Mini"},
-	{Mousse_Cake_a, Mousse_Cake_t, "Mousse_Cake"},
-	{Maple_Shroom_a, Maple_Shroom_t, "Maple_Shroom"},
-	{Maple_Super_a, Maple_Super_t, "Maple_Super"},
-	{Maple_Ultra_a, Maple_Ultra_t, "Maple_Ultra"},
-	{Maple_Syrup_a, Maple_Syrup_t, "Maple_Syrup"},
-	{Fried_Egg_a, Fried_Egg_t, "Fried_Egg"},
-	{Heartful_Cake_a, Heartful_Cake_t, "Heartful_Cake"},
-	{Coconut_a, Coconut_t, "Coconut"},
-	{Snow_Bunny_a, Snow_Bunny_t, "Snow_Bunny"},
-	{Earth_Quake_a, Earth_Quake_t, "Earth_Quake"},
-	{Hot_Sauce_a, Hot_Sauce_t, "Hot_Sauce"},
-	{Jelly_Shroom_a, Jelly_Shroom_t, "Jelly_Shroom"},
-	{Jelly_Super_a, Jelly_Super_t, "Jelly_Super"},
-	{Jelly_Ultra_a, Jelly_Ultra_t, "Jelly_Ultra"},
-	{Jelly_Candy_a, Jelly_Candy_t, "Jelly_Candy"},
-	{Jammin_Jelly_a, Jammin_Jelly_t, "Jammin_Jelly"},
-	{Fresh_Pasta_a, Fresh_Pasta_t, "Fresh_Pasta"}
-};
-
+Type_Sort getTypeKey (Alpha_Sort a_key) {
+	for (int i = Mushroom_t; i <= Mistake_t; i++) {
+		if (items[i].a_key == a_key) {
+			return items[i].t_key;
+		}
+	}
+	
+	return -1;
+}
 
 int **getInventoryFrames() {
 	static int *inv_frames[INVENTORY_MAX_SIZE];
@@ -244,12 +155,38 @@ int **getInventoryFrames() {
 	return inv_frames;
 }
 
+Item *getStartingInventory() {
+	static Item inventory[] = {
+		{Golden_Leaf, Golden_Leaf_t},
+		{Peachy_Peach, Peachy_Peach_t},
+		{Shooting_Star, Shooting_Star_t},
+		{Ultra_Shroom, Ultra_Shroom_t},
+		{Cake_Mix, Cake_Mix_t},
+		{Thunder_Rage, Thunder_Rage_t},
+		{Turtley_Leaf, Turtley_Leaf_t},
+		{Life_Shroom, Life_Shroom_t},
+		{Ice_Storm, Ice_Storm_t},
+		{Slow_Shroom, Slow_Shroom_t},
+		{Mystery, Mystery_t},
+		{Honey_Syrup, Honey_Syrup_t},
+		{Volt_Shroom, Volt_Shroom_t},
+		{Fire_Flower, Fire_Flower_t},
+		{Mystic_Egg, Mystic_Egg_t},
+		{Hot_Dog, Hot_Dog_t},
+		{Ruin_Powder, Ruin_Powder_t},
+		{Maple_Syrup, Maple_Syrup_t},
+		{Hot_Sauce, Hot_Sauce_t},
+		{Jammin_Jelly, Jammin_Jelly_t}
+	};
+	
+	return inventory;
+}
+
 int main()
 {
 	/* FOR TESTING getInventoryFrames
 	int **inv_frames;
 	inv_frames = getInventoryFrames();
-
 	for (int i = 0; i <= INVENTORY_MAX_SIZE; i++) {
 		for (int j = 0; j < i; j++) {
 			int *frames = inv_frames[i];
@@ -257,16 +194,23 @@ int main()
 		}
 	}*/
 	
-	printf("Name\t\tAlpha\tType\n");
 	
 	/* FOR TESTING ITEM SORT RETRIEVAL
+	printf("Name\t\tAlpha\tType\n");
 	for (int i = 0; i < NUM_ITEMS; i++) {
 		char *name = items[i].name;
 		enum Alpha_Sort a_key = items[i].a_key;
 		enum Type_Sort t_key = items[i].t_key;
-		
 		printf("%s\t%d\t%d\n", name, a_key, t_key);
 	}*/
+	
+	
+	/* FOR TESTING KEY RETRIEVAL
+	Item *inventory = getStartingInventory();
+	for (int i = 0; i < 20; i++) {
+		printf("Slot   %d\tAlpha   %d\tType %d\t\n", i+1, inventory[i].a_key, inventory[i].t_key);
+	}
+	*/
 	
 	return 0;
 }
