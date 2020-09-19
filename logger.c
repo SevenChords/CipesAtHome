5f74746af49cda04c4ab597aa5441da675c50480
Malleo <seanowens@comcast.net>
2020-09-19 15:18:44 -0400

    Removed unnecessary critical block which causes Linux build to hang

--- logger.c
+++ logger.c
@@ -13,32 +13,29 @@ int init_level_cfg() {
 }
 
 int recipeLog(int level, char *process, char *subProcess, char *activity, char *entry) {
-	#pragma omp critical
-	{
-		if (level_cfg >= level) {
-			int hours, mins, secs, month, day, year;
-			time_t now;
-			time(&now);
-			struct tm* local = localtime(&now);
-			hours = local->tm_hour;
-			mins = local->tm_min;
-			secs = local->tm_sec;
-			day = local->tm_mday;
-			month = local->tm_mon + 1;
-			year = local->tm_year + 1900;
-			char date[100];
-			char data[200];
-			sprintf(date, "[%d-%02d-%02d %02d:%02d:%02d]", year, month, day, hours, mins, secs);
-			sprintf(data, "[%s][%s][%s][%s]\n", process, subProcess, activity, entry);
-			printf(date);
-			printf(data);
+	if (level_cfg >= level) {
+		int hours, mins, secs, month, day, year;
+		time_t now;
+		time(&now);
+		struct tm* local = localtime(&now);
+		hours = local->tm_hour;
+		mins = local->tm_min;
+		secs = local->tm_sec;
+		day = local->tm_mday;
+		month = local->tm_mon + 1;
+		year = local->tm_year + 1900;
+		char date[100];
+		char data[200];
+		sprintf(date, "[%d-%02d-%02d %02d:%02d:%02d]", year, month, day, hours, mins, secs);
+		sprintf(data, "[%s][%s][%s][%s]\n", process, subProcess, activity, entry);
+		printf(date);
+		printf(data);
 
-			FILE* fp = fopen("recipes.log", "a");
-			fputs(date, fp);
-			fputs(data, fp);
+		FILE* fp = fopen("recipes.log", "a");
+		fputs(date, fp);
+		fputs(data, fp);
 
-			fclose(fp);
-		}
+		fclose(fp);
 	}
 	return 0;
 }
