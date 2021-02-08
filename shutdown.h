e668cf56348bd7971d70c04d12d4e1753e368edb
C. Sean Young <techsy730@gmail.com>
2021-02-08 10:47:45 -0600

    Add header guards for shutdown.h

--- shutdown.h
+++ shutdown.h
@@ -1,3 +1,6 @@
+#ifndef CRIPES_SHUTDOWN_H
+#define CRIPES_SHUTDOWN_H
+
 #include <stdbool.h>
 #include "absl/base/port.h"
 
@@ -8,4 +11,5 @@ bool requestShutdown();
 ABSL_ATTRIBUTE_ALWAYS_INLINE inline bool askedToShutdown() {
 	return ABSL_PREDICT_FALSE(_askedToShutdownVar);
 }
+#endif
 
