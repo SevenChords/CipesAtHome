379053e8d60b80d311e978e8d277c9ecdbf03db9
C. Sean Young <techsy730@gmail.com>
2021-02-17 11:21:45 -0600

    Move `_IS_WINDOWS` from start.c to base.h, along with `min` and `max`.
    This is a more logical "location" for them as they aren't tied to specific file's logic.
    (This and other revisions to be cherry-picked rely on them)

--- base.h
+++ base.h
@@ -4,6 +4,29 @@
 #include <stdio.h>
 #include "absl/base/port.h"
 
+#if defined(_MSC_FULL_VER) || defined(_MSC_VER) || defined(__MINGW32__)
+#define _IS_WINDOWS 1
+#else
+#define _IS_WINDOWS 0
+#endif
+
+// Microsoft standard library provides these as an extension, but we need definitions for the POSIX side.
+#ifndef min
+// Computes the minimum between two values.
+// If the two values compare equal, which argument is returned is unspecified.
+// WARNING: This implementation takes NO precautions against
+// arguments having side effects or being expensive to compute.
+#define min(x, y) ((y) < (x) ? (y) : (x))
+#endif
+
+#ifndef max
+// Computes the maximum between two value.
+// If the two values compare equal, which argument is returned is unspecified.
+// WARNING: This implementation takes NO precautions against
+// arguments having side effects or being expensive to compute.
+#define max(x, y) ((y) > (x) ? (y) : (x))
+#endif
+
  /*-------------------------------------------------------------------
  * Function 	: checkMallocFailed
  * Inputs	: void* p
--- calculator.c
+++ calculator.c
@@ -53,12 +53,6 @@ ABSL_ATTRIBUTE_ALWAYS_INLINE static inline bool checkShutdownOnIndexLong(long i)
 	return i % CHECK_SHUTDOWN_INTERVAL == 0 && askedToShutdown();
 }
 
-// TEMPORARY WORKAROUND for an unintentionally added implicit reference.
-// When the real min function comes in, replace this definition with that one.
-#ifndef min
-#define min(x, y) ((y) < (x) ? (y) : (x))
-#endif
-
 /*-------------------------------------------------------------------
  * Function 	: initializeInvFrames
  *
